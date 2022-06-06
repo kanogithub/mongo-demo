21b5bbd9ea5333e7da075954543b653705b96d8a
kanogithub <linkano.teck@gmail.com>
2022-06-07 00:01:56 +1000

    mongoose validate

---
+++ async-valid.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+const MONGODB = 'mongodb://localhost:27017/mongo-exercises'
+
+// Mongoose Connection
+mongoose
+	.connect(MONGODB)
+	.then(() => console.log('Connected to MongoDB'))
+	.catch((error) => console.error('Could not connect to MongoDB'))
+
+// Mongoose Schema with Custom Validation
+const courseSchema = new mongoose.Schema({
+	_id: String,
+	name: String,
+	author: String,
+	tags: {
+		type: Array,
+		validate: {
+			isAsync: true,
+			validator: function (v, callback) {
+				// Simulate async works
+				setTimeout(() => {
+					// Do some async work
+					const result = v && v.length > 0
+					callback(result)
+				}, 1000)
+			},
+			message: 'A course should have at least one tag.',
+		},
+	},
+	data: { type: Date, default: Date.now },
+	isPublished: Boolean,
+	price: Number,
+})
+
+// Compile to Model
+const Course = mongoose.model('Course', courseSchema)
+
+// instance and init then save
+async function creatCourse() {
+	const course = new Course({
+		name: 'Angular Course',
+		author: 'Mosh',
+		// tags: ['web'],
+		isPublished: true,
+	})
+
+	try {
+		await course.validate()
+		// const result = await course.save()
+		// console.log(result)
+	} catch (err) {
+		console.log(err.message)
+	}
+}
+
+creatCourse()
